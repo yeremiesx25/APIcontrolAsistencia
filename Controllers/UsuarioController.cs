447db33a0cf2537f62b160ed9bffabecb9c0e7ad
yeremiesx25 <j4r4my_2002@hotmail.com>
2024-08-10 01:30:10 -0500

    a

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -20,8 +20,8 @@ namespace APIcontrolAsistencia.Controllers
         }
 
         [HttpGet]//obtener
-        [Route("Usuario")]
-        public IActionResult Usuario() { 
+        [Route("MostrarUsuario")]
+        public IActionResult MostrarUsuario() { 
             List<usuario> usuarioLista = new List<usuario>();
             try {
                 using (var conexion = new SqlConnection(cadenaSQL)) {//conectando a la base de datos
@@ -31,7 +31,7 @@ namespace APIcontrolAsistencia.Controllers
                     using (var reader = cmd.ExecuteReader()) {//leer el store
                         while (reader.Read()) {
                             usuarioLista.Add(new usuario() {
-                                DNI = Convert.ToInt32(reader["DNI"]),
+                                DNI = Convert.ToInt32(reader["DNI"]),//tiene que leer lo que sale en la tabla al mostrar
                                 CONTRASENA = reader["CONTRASENA"].ToString(),
                                 Nombres = reader["Nombres"].ToString(),
                                 Apellidos = reader["Apellidos"].ToString(),
@@ -50,5 +50,120 @@ namespace APIcontrolAsistencia.Controllers
 
             }
         }
+
+
+        //buscar usuario
+        [HttpGet]//obtener
+        [Route("BuscarUsuario/{DNI:int}")]
+        public IActionResult BuscarUsuario(int DNI)
+        {
+            List<usuario> usuarioLista = new List<usuario>();
+            usuario usuarioBsq = new usuario();
+            try
+            {
+                using (var conexion = new SqlConnection(cadenaSQL))
+                {//conectando a la base de datos
+                    conexion.Open();//iniciando
+                    var cmd = new SqlCommand("SP_USUARIO", conexion);//llamando al store
+                    cmd.CommandType = CommandType.StoredProcedure;//diciendo que es un store
+                    using (var reader = cmd.ExecuteReader())
+                    {//leer el store
+                        while (reader.Read())
+                        {
+                            usuarioLista.Add(new usuario()
+                            {
+                                DNI = Convert.ToInt32(reader["DNI"]),//tiene que leer lo que sale en la tabla al mostrar
+                                CONTRASENA = reader["CONTRASENA"].ToString(),
+                                Nombres = reader["Nombres"].ToString(),
+                                Apellidos = reader["Apellidos"].ToString(),
+                                Email = reader["Email"].ToString(),
+                                Telefono = reader["Telefono"].ToString(),
+                                InicioDePracticas = Convert.ToDateTime(reader["InicioDePracticas"]),
+                                TipoDeUsuario = Convert.ToInt32(reader["TipoDeUsuario"])
+                            });
+                        }
+                    }
+                }
+                usuarioBsq = usuarioLista.Where(item => item.DNI == DNI).FirstOrDefault();
+
+                return StatusCode(StatusCodes.Status200OK, new { mensaje = "ok", response = usuarioBsq });
+
+            }
+            catch (Exception error)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { mensaje = error.Message, response = usuarioBsq });
+
+            }
+        }
+
+        //Crear Usuario
+        [HttpPost]//MANDAR
+        [Route("CrearUsuario")]
+        public IActionResult CrearUsuario([FromBody] usuario objeto)
+        {
+            try
+            {
+                using (var conexion = new SqlConnection(cadenaSQL))
+                {//conectando a la base de datos
+                    conexion.Open();//iniciando
+                    var cmd = new SqlCommand("SP_CrearUsuario", conexion);//llamando al store
+                    cmd.Parameters.AddWithValue("DNI", objeto.DNI);
+                    cmd.Parameters.AddWithValue("CONTRASENA", objeto.CONTRASENA);
+                    cmd.Parameters.AddWithValue("Nombres", objeto.Nombres);
+                    cmd.Parameters.AddWithValue("Apellidos", objeto.Apellidos);
+                    cmd.Parameters.AddWithValue("Email", objeto.Email);
+                    cmd.Parameters.AddWithValue("Telefono", objeto.Telefono);
+                    cmd.Parameters.AddWithValue("DepartmentID", objeto.DepartmentID);
+                    cmd.Parameters.AddWithValue("TipoDeUsuario", objeto.TipoDeUsuario);
+                    cmd.CommandType = CommandType.StoredProcedure;//diciendo que es un store
+                    
+                    cmd.ExecuteNonQuery();
+                }
+
+                return StatusCode(StatusCodes.Status200OK, new { mensaje = "Agregado Correctamente"});
+
+            }
+            catch (Exception error)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { mensaje = error.Message});
+
+            }
+        }
+
+        //EDITAR EL USUARIO
+        [HttpPut]//MANDAR
+        [Route("EditarUsuario")]
+        public IActionResult EditarUsuario([FromBody] usuario objeto)
+        {
+            try
+            {
+                using (var conexion = new SqlConnection(cadenaSQL))
+                {//conectando a la base de datos
+                    conexion.Open();//iniciando
+                    var cmd = new SqlCommand("SP_EditarUsuario", conexion);//llamando al store
+                    cmd.Parameters.AddWithValue("DNI", objeto.DNI);
+                    cmd.Parameters.AddWithValue("CONTRASENA", objeto.CONTRASENA is null ? DBNull.Value : objeto.CONTRASENA);//si manda vacio se quedara con el valor que ya tiene
+                    cmd.Parameters.AddWithValue("Nombres", objeto.Nombres is null ? DBNull.Value : objeto.Nombres);
+                    cmd.Parameters.AddWithValue("Apellidos", objeto.Apellidos is null ? DBNull.Value : objeto.Apellidos);
+                    cmd.Parameters.AddWithValue("Email", objeto.Email is null ? DBNull.Value : objeto.Email);
+                    cmd.Parameters.AddWithValue("Telefono", objeto.Telefono is null ? DBNull.Value : objeto.Telefono);
+                    cmd.Parameters.AddWithValue("DepartmentID", objeto.DepartmentID == 0 ? DBNull.Value : objeto.DepartmentID);
+                    cmd.Parameters.AddWithValue("TipoDeUsuario", objeto.TipoDeUsuario == 0 ? DBNull.Value : objeto.TipoDeUsuario);
+                    cmd.CommandType = CommandType.StoredProcedure;//diciendo que es un store
+
+                    cmd.ExecuteNonQuery();
+                }
+
+                return StatusCode(StatusCodes.Status200OK, new { mensaje = "Editado Correctamente" });
+
+            }
+            catch (Exception error)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { mensaje = error.Message });
+
+            }
+        }
+
+
     }
 }
